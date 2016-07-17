00cfc967cc2d6aa222afd322cc3079ff73a94c0c
Colin Monroe <cmmonroe2010@gmail.com>
2016-07-17 13:52:20 -0400

    Initial Commit.

---
+++ Controllers/PortalController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResidentPortal.Controllers
+{
+    public class PortalController : Controller
+    {
+        // GET: Portal
+        public ActionResult Home()
+        {
+            return View();
+        }
+        //GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+        //GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/PortalModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResidentPortal.Models
+{
+    public class PortalModel
+    {
+
+    }
+}
